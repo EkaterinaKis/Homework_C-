ef3e69a3a0ea27549c0bb1d37736079d86210282
Ekaterina Kiseleva <kiselevakat94@bk.ru>
2022-09-20 01:17:25 +0300

    Added homework

---
+++ 01/Program.cs
@@ -0,0 +1,62 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*Console.WriteLine("Введите первое число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number > number2)
+{
+    Console.WriteLine($"число {number} > числа {number2}");
+}
+else
+{
+    Console.WriteLine($"число {number2} > числа {number}");
+}*/
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*Console.WriteLine("Введите первое число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+if (number > number2 && number > number3)
+{
+    Console.WriteLine($"max = {number}");
+}
+else
+{
+    if (number2 > number3)
+{
+    Console.WriteLine($"max = {number2}");
+}
+else
+{
+    Console.WriteLine($"max = {number3}");
+}
+}*/
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число является нечётным");
+}*/
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+/*Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int a = 2;
+while (a <= number)
+{
+    Console.Write(a + " ");
+    a = a + 2;
+}*/
